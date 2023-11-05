5caeeb7e1178b6f83b3aaa3849e937d42914653d
Marc Orfila Carreras <marcorfila2003@gmail.com>
2023-11-05 15:32:16 +0100

    fix: Changed the response code

--- src/main/java/cloud/marcorfilacarreras/matemaquest/common/NotFoundHandler.java
+++ src/main/java/cloud/marcorfilacarreras/matemaquest/common/NotFoundHandler.java
@@ -9,6 +9,7 @@ public class NotFoundHandler implements Route {
     @Override
     public Object handle(Request request, Response response) throws Exception {
         response.type("application/json");
+        response.status(404);
         
         return "{\"status\":\"fail\",\"data\":{\"message\":\"Not found.\"}}";
     }
