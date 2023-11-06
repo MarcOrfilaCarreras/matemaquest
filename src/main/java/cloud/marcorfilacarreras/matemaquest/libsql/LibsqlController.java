3cedc220a3a2504ee65b207741fb1842b8f3ce17
Marc Orfila Carreras <marcorfila2003@gmail.com>
2023-11-06 15:41:36 +0100

    fix: Removed the Redis code

--- src/main/java/cloud/marcorfilacarreras/matemaquest/libsql/LibsqlController.java
+++ src/main/java/cloud/marcorfilacarreras/matemaquest/libsql/LibsqlController.java
@@ -2,7 +2,6 @@ package cloud.marcorfilacarreras.matemaquest.libsql;
 
 import cloud.marcorfilacarreras.matemaquest.models.Exam;
 import cloud.marcorfilacarreras.matemaquest.models.Question;
-import cloud.marcorfilacarreras.matemaquest.redis.RedisController;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,7 +9,6 @@ public class LibsqlController {
     
     private final String url = System.getenv("LIBSQL_URL");
     private final String authToken = System.getenv("LIBSQL_TOKEN");
-    private final static RedisController redis = new RedisController();
 
     public LibsqlController() {
     }
@@ -56,8 +54,6 @@ public class LibsqlController {
             
             exam.appendQuestion(q);
         }
-        
-        redis.saveExam(id, exam.toJson());
 
         return exam;
     }
@@ -98,8 +94,6 @@ public class LibsqlController {
                     (String) row.get(7)
             );
         }
-        
-        redis.saveQuestion(question.getId(), question.toJson());
 
         return question;
     }
