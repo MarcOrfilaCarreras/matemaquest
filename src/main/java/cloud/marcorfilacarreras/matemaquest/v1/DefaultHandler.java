539bb7a2a7f86dcefedc9f73f61161ccf4e9e09a
Marc Orfila Carreras <marcorfila2003@gmail.com>
2023-11-22 20:00:39 +0100

    feat: Update endpoint behavior to display structured JSON response containing information about various endpoints

--- src/main/java/cloud/marcorfilacarreras/matemaquest/v1/DefaultHandler.java
+++ src/main/java/cloud/marcorfilacarreras/matemaquest/v1/DefaultHandler.java
@@ -11,7 +11,42 @@ public class DefaultHandler implements Route {
     
     @Override
     public Object handle(Request request, Response response) throws Exception {
-        response.redirect("/v1/exam/");
+        // Setting response type to JSON
+        response.type("application/json");
+        
+        response.status(200);
+        response.body(
+                "{\n" +
+                "  \"data\": {\n" +
+                "    \"endpoints\": [\n" +
+                "      {\n" +
+                "        \"url\": \"/v1\",\n" +
+                "        \"method\": \"GET\"\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"url\": \"/v1/exam/:id\",\n" +
+                "        \"method\": \"GET\"\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"url\": \"/v1/search\",\n" +
+                "        \"method\": \"GET\"\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"url\": \"/v1/question/:id\",\n" +
+                "        \"method\": \"GET\"\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"url\": \"/v1/question/:id/image/exercise\",\n" +
+                "        \"method\": \"GET\"\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"url\": \"/v1/question/:id/image/answer\",\n" +
+                "        \"method\": \"GET\"\n" +
+                "      }\n" +
+                "    ]\n" +
+                "  },\n" +
+                "    \"status\": \"success\"\n" +
+                "}");
         return null;
     }
 }
